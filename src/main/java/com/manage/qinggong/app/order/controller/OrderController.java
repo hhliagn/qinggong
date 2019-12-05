c640a8635271d1d36b19877d1aa5a26d9180f865
lianghenghui <lianghenghui@ice-point.com.cn>
2019-12-05 20:13:17 +0800

    预约 fix

--- src/main/java/com/manage/qinggong/app/order/controller/OrderController.java
+++ src/main/java/com/manage/qinggong/app/order/controller/OrderController.java
@@ -24,16 +24,8 @@ public class OrderController {
     @PostMapping(value = "")
     public Response submitOrder(@RequestBody Order order){
         if (order == null) return new Response("预约信息为空");
-        //加判断，是否该时间可以预约 并且 该时间段人数未满
-        Date orderDate = order.getOrderDate();
-        String orderTime = order.getOrderTime();
-        String date = DateUtils.dateToStr(orderDate, "yyyy-MM-dd");
-        Integer dayOfWeek = DateUtils.dayOfWeek(orderDate);
-        //查询开馆时间 place_setup open_time
-
-        //插入记录
-
-        //生成二维码
-        return null;
+        boolean flag = orderService.submitOrder(order);
+        if (flag) return new Response("提交预约成功", flag);
+        return new Response("提交预约失败", flag);
     }
 }
--- src/main/java/com/manage/qinggong/app/order/service/OrderService.java
+++ src/main/java/com/manage/qinggong/app/order/service/OrderService.java
@@ -1,7 +1,25 @@
 package com.manage.qinggong.app.order.service;
 
+import com.manage.qinggong.app.order.pojo.Order;
+import com.manage.qinggong.base.DateUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class OrderService {
+    public boolean submitOrder(Order order) {
+        //加判断，是否该时间可以预约 并且 该时间段人数未满
+        Date orderDate = order.getOrderDate();
+        String orderTime = order.getOrderTime();
+        String date = DateUtils.dateToStr(orderDate, "yyyy-MM-dd");
+        Integer dayOfWeek = DateUtils.dayOfWeek(orderDate);
+        //查询开馆时间 place_setup open_time
+
+        //插入记录
+
+        //生成二维码
+
+        return false;
+    }
 }
